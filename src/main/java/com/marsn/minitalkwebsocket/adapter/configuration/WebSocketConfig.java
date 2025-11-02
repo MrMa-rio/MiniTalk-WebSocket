87ae2c7d32bcee227fc584700a8cdbc02867ed66
Mario Alberto <112214759+MrMa-rio@users.noreply.github.com>
2025-11-02 12:41:16 -0300

    feat: criando conexao com websocket

--- src/main/java/com/marsn/minitalkwebsocket/adapter/configuration/WebSocketConfig.java
+++ src/main/java/com/marsn/minitalkwebsocket/adapter/configuration/WebSocketConfig.java
@@ -1,20 +1,26 @@
 package com.marsn.minitalkwebsocket.adapter.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        // Prefixo para mensagens destinadas ao servidor (Controller)
         config.setApplicationDestinationPrefixes("/app");
+        // Prefixo para o broker (mensagens enviadas do servidor para o cliente)
+        config.enableSimpleBroker("/topic");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        // Endpoint que o cliente usará para se conectar ao WebSocket
+        registry.addEndpoint("/ws-chat").withSockJS();
     }
 }
--- src/main/java/com/marsn/minitalkwebsocket/entrypoint/websocket/WebSocketController.java
+++ src/main/java/com/marsn/minitalkwebsocket/entrypoint/websocket/WebSocketController.java
@@ -1,15 +1,18 @@
 package com.marsn.minitalkwebsocket.entrypoint.websocket;
 
-import com.marsn.minitalkwebsocket.entrypoint.websocket.dto.ChatMessageDTO;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
-    @MessageMapping("/chat")
-    @SendTo("/topic/chat")
-    public ChatMessageDTO sendMessage(ChatMessageDTO message) {
-        return message;
+
+    // Mapeia mensagens enviadas para /app/message
+    @MessageMapping("/message")
+    // Envia o retorno para todos os assinantes de /topic/messages
+    @SendTo("/topic/messages")
+    public String handleMessage(String message) {
+        System.out.println("Mensagem recebida: " + message);
+        return "Servidor: Recebi sua mensagem: '" + message + "'";
     }
 }
